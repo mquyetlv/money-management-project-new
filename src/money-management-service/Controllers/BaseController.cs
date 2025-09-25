3ffe6918b04ab19a6c8360591573b29f34a9e8b5
quyetlv <m.quyetlv@gmail.com>
2025-09-25 16:41:36 +0700

    Update base controller

--- src/money-management-service/Controllers/BaseController.cs
+++ src/money-management-service/Controllers/BaseController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace money_management_service.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
     public class BaseController : ControllerBase
     {
     }
