2db698d8295a55cb22c89c84cfdfcf42b29aca2e
quyetlv <m.quyetlv@gmail.com>
2025-10-03 14:17:53 +0700

    header

--- src/money-management-fe/src/layouts/header/header.ts
+++ src/money-management-fe/src/layouts/header/header.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MonInout } from '../../core/components/input/input';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MonInout,
+  ],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class Header {
 
--- src/money-management-fe/src/modules/transactions/pages/overview/overview.ts
+++ src/money-management-fe/src/modules/transactions/pages/overview/overview.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-overview',
-  imports: [],
+  imports: [
+  ],
   templateUrl: './overview.html',
   styleUrl: './overview.css'
 })
 export class Overview {
+  domSantize = inject(DomSanitizer);
 
+  ngOnInit() {
+    this.domSantize
+  }
 }
