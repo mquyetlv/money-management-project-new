5649b07ed078f0861f548c234a4171bc8bffbfd0
quyetlv <m.quyetlv@gmail.com>
2025-10-06 16:06:56 +0700

    Sample table

--- src/money-management-fe/src/core/interfaces/column-table.interface.ts
+++ src/money-management-fe/src/core/interfaces/column-table.interface.ts
@@ -1,6 +1,10 @@
 export interface ColumnTable {
     headerName: string,
     key: string,
-    showSort?: boolean,
+    canSort?: boolean,
     type?: 'DATE' | 'MONEY',
+    width?: string,
+    headerClass?: string,
+    columnClass?: string,
+    align?: 'left' | 'center' | 'right',
 }
--- src/money-management-fe/src/modules/transactions/pages/overview/overview.ts
+++ src/money-management-fe/src/modules/transactions/pages/overview/overview.ts
@@ -15,7 +15,26 @@ export class Overview {
   domSantize = inject(DomSanitizer);
 
   columns: ColumnTable[] = [
-    { headerName: "User name", key: "" }
+    { headerName: "User name", key: "username" },
+    { headerName: "Full name", key: "fullname" },
+    { headerName: "Email", key: "email" },
+    { headerName: "Phone number", key: "phoneNumber", align: 'right' },
+    { headerName: "Something", key: "something" },
+  ];
+
+  data: any = [
+    { username: "quyetlv1", fullname: "Le Van Quyet1", email: "lequyet1@gmail.com" , phoneNumber: "09432432424", something: "something1" },
+    { username: "quyetlv2", fullname: "Le Van Quyet2", email: "lequyet2@gmail.com", phoneNumber: "094324324242", something: "something2" },
+    { username: "quyetlv3", fullname: "Le Van Quyet3", email: "lequyet3@gmail.com", phoneNumber: "094324324243", something: "something3" },
+    { username: "quyetlv4", fullname: "Le Van Quyet4", email: "lequyet4@gmail.com", phoneNumber: "094324324244", something: "something4" },
+    { username: "quyetlv4", fullname: "Le Van Quyet1", email: "lequyet1@gmail.com", phoneNumber: "094324324241", something: "something1" },
+    { username: "quyetlv5", fullname: "Le Van Quyet5", email: "lequyet5@gmail.com", phoneNumber: "094324324245", something: "something5" },
+    { username: "quyetlv6", fullname: "Le Van Quyet6", email: "lequyet6@gmail.com", phoneNumber: "094324324246", something: "something6" },
+    { username: "quyetlv7", fullname: "Le Van Quyet7", email: "lequyet7@gmail.com", phoneNumber: "094324324247", something: "something7" },
+    { username: "quyetlv8", fullname: "Le Van Quyet8", email: "lequyet8@gmail.com", phoneNumber: "094324324248", something: "something8" },
+    { username: "quyetlv9", fullname: "Le Van Quyet9", email: "lequyet9@gmail.com", phoneNumber: "094324324249", something: "something9" },
+    { username: "quyetlv10", fullname: "Le Van Quyet10", email: "lequyet10@gmail.com", phoneNumber: "0943243242410", something: "something10" },
+    { username: "quyetlv11", fullname: "Le Van Quyet11", email: "lequyet11@gmail.com", phoneNumber: "0943243242411", something: "something11" },
   ]
 
   ngOnInit() {
